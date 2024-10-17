3e1deb93c21cdb54ec95ac13d145f4a2fe1d2f38
Rokuta-Kaoru <e9409@g.maizuru-ct.ac.jp>
2024-10-17 23:10:28 +0900

    replace .gitginore
    .gitignore移動したで

--- Puranabi/OpenCV/build/intermediates/cxx/Debug/4al2mi6v/logs/x86_64/build_command_opencv_jni_shared.bat
+++ Puranabi/OpenCV/build/intermediates/cxx/Debug/4al2mi6v/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\\Users\\syake\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "C:\\Users\\syake\\AndroidStudioProjects\\Puranabi\\opencv\\.cxx\\Debug\\4al2mi6v\\x86_64" ^
+  "C:\\Users\\syake\\GitHub\\changes_checker\\Puranabi\\opencv\\.cxx\\Debug\\4al2mi6v\\x86_64" ^
   opencv_jni_shared
--- Puranabi/OpenCV/build/intermediates/cxx/Debug/4al2mi6v/logs/x86_64/configure_command.bat
+++ Puranabi/OpenCV/build/intermediates/cxx/Debug/4al2mi6v/logs/x86_64/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Users\\syake\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Users\\syake\\AndroidStudioProjects\\Puranabi\\opencv\\libcxx_helper" ^
+  "-HC:\\Users\\syake\\GitHub\\changes_checker\\Puranabi\\opencv\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=33" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=C:\\Users\\syake\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\syake\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\syake\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\syake\\AndroidStudioProjects\\Puranabi\\opencv\\build\\intermediates\\cxx\\Debug\\4al2mi6v\\obj\\x86_64" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\syake\\AndroidStudioProjects\\Puranabi\\opencv\\build\\intermediates\\cxx\\Debug\\4al2mi6v\\obj\\x86_64" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\syake\\GitHub\\changes_checker\\Puranabi\\opencv\\build\\intermediates\\cxx\\Debug\\4al2mi6v\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\syake\\GitHub\\changes_checker\\Puranabi\\opencv\\build\\intermediates\\cxx\\Debug\\4al2mi6v\\obj\\x86_64" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Users\\syake\\AndroidStudioProjects\\Puranabi\\opencv\\.cxx\\Debug\\4al2mi6v\\x86_64" ^
+  "-BC:\\Users\\syake\\GitHub\\changes_checker\\Puranabi\\opencv\\.cxx\\Debug\\4al2mi6v\\x86_64" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
